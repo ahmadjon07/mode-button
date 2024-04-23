de3ebafd0c1e601d2aea0cbb62d96f99852e98fd
ahmadjon07 <ahmadzonzulunov1@gmail.com>
2024-04-23 17:39:02 +0300

    btn

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './App.scss';
+
+function App() {
+
+  const [mode, setMode] = useState(true)
+  return (
+    <div className={mode ? "App light" : "App dark"}>
+      <h1 className={mode == true ? 'title light': 'title dark'}>Form</h1>
+      <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1"/>
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<button onClick={()=> setMode(!mode)}>
+    <img width={70} src={mode ? "https://png.pngtree.com/png-clipart/20201209/original/pngtree-sun-png-clipart-colored-png-image_5656301.png" : "https://png.pngtree.com/png-clipart/20210310/original/pngtree-the-surface-of-a-round-moon-png-image_5955908.png"} alt="" />
+</button>
+    </div>
+  );
+}
+
+export default App;
